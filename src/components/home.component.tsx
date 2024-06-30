47515caddb910f641481ccc0f11da2d04eb487aa
AyalaDiamant <Ayala66610@gmail.com>
2024-06-30 15:42:08 +0300

    fix home component

--- src/components/home.component.tsx
+++ src/components/home.component.tsx
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { logout } from '../api/login.api';
 
 const Home = () => {
-    let userName = localStorage.getItem('user-name');
-    const logoutFunc = () =>{
-        logout();
-        alert('logout successful'); 
-        localStorage.clear();
-        userName = null;
-    }
+    const [userName, setUserName] = useState('');
 
+    useEffect(() => {
+        const storedUserName = localStorage.getItem('user-name');
+        if (storedUserName) {
+            setUserName(storedUserName);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('user-name');
+        setUserName('');
+    };
 
     return (
         <div>
-            <h1>ברוכים הבאים לאתר שלנו</h1>
-            <p>אנחנו מתמחים בפאות איכותיות ומגוון שירותים נלווים.</p>
+            <h1>Welcome to our site</h1>
+            <p>We specialize in quality wigs and a variety of related services.</p>
             <div>
                 <button><Link to={'/sign-up'}>To register</Link></button>
-                <button><Link to={'/sign-in'}>To connect</Link></button> 
-                {userName ? <button onClick={logoutFunc}><Link to={'/'}>To logout</Link></button> : <h1></h1>}
-                {userName ? <h1>ברוך הבא, {userName}!</h1> : <h1></h1>}
-                {userName ? <h5>השירותים שלנו <button><Link to={'/services'}>our services</Link></button></h5> : <h1></h1>}
-                {userName ? <h5>הפגישות שלך<button><Link to={'/meeting'}>your meeting</Link></button></h5> : <h1></h1>}
+                <button><Link to={'/sign-in'}>To connect</Link></button>
+                {userName ? (
+                    <>
+                        <button onClick={handleLogout}>To logout</button>
+                        <h1>Welcome, {userName}!</h1>
+                        <h3><button><Link to={'/services'}>our services</Link></button></h3>
+                        <h3><button><Link to={'/meeting'}>your meeting</Link></button></h3>
+                    </>
+                ) : null}
             </div>
         </div>
     );
 };
 
 export default Home;
+
